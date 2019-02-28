8225461cc054030b0b0f17732bee3be3c6b84d6a
duonganh <anh.d@shopstack.asia>
2019-02-28 14:18:21 +0700

    Updates

---
+++ precache-manifest.3a7588e3058f5dd5bf6844356bdb0cd1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello-reactjs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3e1c947dc6c423ad7b0bd0fff30bf8c3",
+    "url": "/hello-reactjs/static/media/anh_2.3e1c947d.JPG"
+  },
+  {
+    "revision": "1898195e9764971e6e61a3d803c5f7eb",
+    "url": "/hello-reactjs/static/media/anh_1.1898195e.JPG"
+  },
+  {
+    "revision": "6840d8b86102102061cf",
+    "url": "/hello-reactjs/static/js/runtime~main.6840d8b8.js"
+  },
+  {
+    "revision": "22b9a3493fdce679f43d",
+    "url": "/hello-reactjs/static/js/main.22b9a349.chunk.js"
+  },
+  {
+    "revision": "170ca1e5011a4b02d05d",
+    "url": "/hello-reactjs/static/js/2.170ca1e5.chunk.js"
+  },
+  {
+    "revision": "22b9a3493fdce679f43d",
+    "url": "/hello-reactjs/static/css/main.77457367.chunk.css"
+  },
+  {
+    "revision": "d22eb6383264c579bb0ac5b6b8f86507",
+    "url": "/hello-reactjs/index.html"
+  }
+];
